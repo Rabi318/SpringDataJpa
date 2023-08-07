8dc3d2e658bc278abb3484e85b305d4d465fd757
RabinarayanS <rabinarayansahoo9348@gamil.com>
2023-08-07 22:57:53 +0530

    Second Commit

--- src/main/java/com/becoder/Recipe.java
+++ src/main/java/com/becoder/Recipe.java
@@ -9,7 +9,7 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
-
+//forTest
     private String description;
     private  Integer prepTime;
     private Integer cookTime;
